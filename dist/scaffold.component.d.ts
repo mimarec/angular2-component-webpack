919e87a485ad5ea838d7a57481c56d6e662ce85c
Pablo Lazaro <pablo.lazaro@gft.com>
2016-09-14 11:34:35 +0200

    chore(dist): delete dist

--- dist/scaffold.component.d.ts
+++
@@ -1,5 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class ScaffoldComponent implements OnInit {
-    constructor();
-    ngOnInit(): void;
-}
--- index.js
+++
@@ -1,2 +0,0 @@
-exports.ScaffoldComponent = require('./dist/scaffold.component.js').ScaffoldComponent;
-exports.ScaffoldModule = require('./dist/scaffold.module.js').ScaffoldModule;
